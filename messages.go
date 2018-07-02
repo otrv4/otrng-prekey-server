9ea92ec4250937c917bad00aaf3897ad15407a95
Ola Bini <ola@olabini.se>
2018-07-02 13:22:38 -0500

    Serialize and deserialize storage information request message

--- messages.go
+++ messages.go
@@ -31,15 +31,21 @@ type storageInformationRequestMessage struct {
 }
 
 func (m *storageInformationRequestMessage) serialize() []byte {
-	// TODO: implement
-	panic("implement me")
-	return nil
+	out := appendShort(nil, version)
+	out = append(out, messageTypeStorageInformationRequest)
+	out = append(out, m.mac[:]...)
+	return out
 }
 
-func (m *storageInformationRequestMessage) deserialize([]byte) ([]byte, bool) {
-	// TODO: implement
-	panic("implement me")
-	return nil, false
+func (m *storageInformationRequestMessage) deserialize(buf []byte) ([]byte, bool) {
+	buf, _, _ = extractShort(buf) // version
+	buf = buf[1:]                 // message type
+
+	var tmp []byte
+	buf, tmp, _ = extractFixedData(buf, 64)
+	copy(m.mac[:], tmp)
+
+	return buf, true
 }
 
 type storageStatusMessage struct {
