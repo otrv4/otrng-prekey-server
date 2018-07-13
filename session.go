2cce420a5a6ede022da1691c366469b1259f7069
Ola Bini <ola@olabini.se>
2018-07-13 16:55:38 -0500

    Add thread safety to session management

--- session.go
+++ session.go
@@ -1,15 +1,15 @@
 package prekeyserver
 
 import (
+	"sync"
 	"time"
 
 	"github.com/otrv4/ed448"
 )
 
-// TODO: thread safety
-
 type sessionManager struct {
 	s map[string]*realSession
+	sync.RWMutex
 }
 
 type session interface {
@@ -29,13 +29,18 @@ type realSession struct {
 	cp          *clientProfile
 	storedMac   []byte
 	lastTouched time.Time
+	sync.Mutex
 }
 
+// expects the lock to be held
 func (s *realSession) touch() {
 	s.lastTouched = time.Now()
 }
 
 func (s *realSession) save(kp *keypair, i ed448.Point, tag uint32, cp *clientProfile) {
+	s.Lock()
+	defer s.Unlock()
+
 	s.touch()
 	s.s = kp
 	s.i = i
@@ -44,26 +49,41 @@ func (s *realSession) save(kp *keypair, i ed448.Point, tag uint32, cp *clientPro
 }
 
 func (s *realSession) instanceTag() uint32 {
+	s.Lock()
+	defer s.Unlock()
+
 	s.touch()
 	return s.tag
 }
 
 func (s *realSession) clientProfile() *clientProfile {
+	s.Lock()
+	defer s.Unlock()
+
 	s.touch()
 	return s.cp
 }
 
 func (s *realSession) pointI() ed448.Point {
+	s.Lock()
+	defer s.Unlock()
+
 	s.touch()
 	return s.i
 }
 
 func (s *realSession) keypairS() *keypair {
+	s.Lock()
+	defer s.Unlock()
+
 	s.touch()
 	return s.s
 }
 
 func (s *realSession) macKey() []byte {
+	s.Lock()
+	defer s.Unlock()
+
 	s.touch()
 	if s.storedMac != nil {
 		return s.storedMac
@@ -72,6 +92,9 @@ func (s *realSession) macKey() []byte {
 }
 
 func (s *realSession) hasExpired(timeout time.Duration) bool {
+	s.Lock()
+	defer s.Unlock()
+
 	return s.lastTouched.Add(timeout).Before(time.Now())
 }
 
@@ -82,24 +105,37 @@ func newSessionManager() *sessionManager {
 }
 
 func (sm *sessionManager) get(name string) session {
+	sm.RLock()
 	s, ok := sm.s[name]
+	sm.RUnlock()
 	if !ok {
 		s = &realSession{}
+		sm.Lock()
 		sm.s[name] = s
+		sm.Unlock()
 	}
 	return s
 }
 
 func (sm *sessionManager) complete(name string) {
+	sm.Lock()
+	defer sm.Unlock()
+
 	delete(sm.s, name)
 }
 
 func (sm *sessionManager) has(name string) bool {
+	sm.RLock()
+	defer sm.RUnlock()
+
 	_, ok := sm.s[name]
 	return ok
 }
 
 func (sm *sessionManager) cleanup(timeout time.Duration) {
+	sm.Lock()
+	defer sm.Unlock()
+
 	toRemove := []string{}
 	for nm, s := range sm.s {
 		if s.hasExpired(timeout) {
