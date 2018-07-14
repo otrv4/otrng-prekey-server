95b9355c4103b297dc99829e2447b3387b78d9b5
Ola Bini <ola@olabini.se>
2018-07-13 19:23:05 -0500

    Add initial command line options

--- server/raw/cmd.go
+++ server/raw/cmd.go
@@ -1,12 +1,15 @@
 package main
 
-// This file contains all the command line options
+import "flag"
 
-// - where to read keys
-// - what port to listen to
-// - what IP to listen to
-// - what storage engine to use - only one available
-// - server identity
-// - fragmentation length
-// - session timeout
-// - fragmentation timeout
+// These flags represent all the available command line flags
+var (
+	keyFile              = flag.String("key-file", "raw-server.keys", "Location of file where server long term keys should be stored and loaded")
+	listenPort           = flag.Uint("port", 3242, "Port to listen to")
+	listenIP             = flag.String("address", "localhost", "Address to listen to")
+	storageEngine        = flag.String("storage", "in-memory", "What storage engine to use: 'in-memory' is the only valid choice at the moment")
+	serverIdentity       = flag.String("identity", "keys.example.org", "The identity of the server")
+	fragLen              = flag.Uint("fragmentation-length", 0, "Fragmentation length - 0 means no fragmenting")
+	sessionTimeout       = flag.Uint("session-timeout", 5, "Session timeout, in minutes")
+	fragmentationTimeout = flag.Uint("fragmentation-timeout", 5, "Fragment timeout, in minutes")
+)
--- server/raw/main.go
+++ server/raw/main.go
@@ -1,9 +1,12 @@
 package main
 
-// We need a command line interface with arguments
+import "flag"
+
 // We need a way of generating keys for the server, and read/write it
 // We need a TCP listener
 // We should print the fingerprint when starting
 
 func main() {
+	flag.Parse()
+
 }
