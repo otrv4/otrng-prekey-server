83fe40ed14c73620fb7f632577ed7ca68b550735
Ola Bini <ola@olabini.se>
2018-07-14 18:01:20 -0500

    Finish basic implementation of the Raw server

--- server/raw/main.go
+++ server/raw/main.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net"
 	"time"
 
 	pks "github.com/otrv4/otrng-prekey-server"
@@ -27,6 +29,36 @@ func main() {
 	listenWith(server)
 }
 
-func listenWith(s pks.Server) {
-	// TODO: implement TCP listener here
+func listenWith(s pks.Server) error {
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *listenIP, *listenPort))
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return err
+		}
+		go handleRequest(conn, s)
+	}
+}
+
+func handleRequest(c net.Conn, s pks.Server) {
+	defer c.Close()
+	data, e := ioutil.ReadAll(c)
+	if e != nil {
+		// print e
+		return
+	}
+	res, e := protocolHandleData(data, s)
+	if e != nil {
+		// print e
+		return
+	}
+	_, e = c.Write(res)
+	if e != nil {
+		// print e
+		return
+	}
 }
--- server/raw/protocol.go
+++ server/raw/protocol.go
@@ -1,3 +1,84 @@
 package main
 
+import (
+	"errors"
+
+	pks "github.com/otrv4/otrng-prekey-server"
+)
+
 // Define the protocol, both incoming and outgoing
+// This protocol has a fragmentation length of 2**16
+// OK, on incoming, what we expect is this:
+// - 2 bytes uint16 len1
+// - len1 bytes containing the "from"
+// - 2 bytes uint16 len2
+// - len2 bytes containing the message
+// Several of these messages can be coming in in the same TCP packet
+// On outgoing, we do the same thing, except we only will send
+// data elements, no "from" elements
+
+type protocolElement struct {
+	from string
+	data string
+}
+
+func protocolHandleData(data []byte, s pks.Server) ([]byte, error) {
+	res, e := protocolParseData(data)
+	if e != nil {
+		return nil, e
+	}
+	result := []byte{}
+	for _, pe := range res {
+		outp, e := s.Handle(pe.from, pe.data)
+		if e != nil {
+			return nil, e
+		}
+		for _, o := range outp {
+			result = append(result, []byte(o)...)
+		}
+	}
+	return result, nil
+}
+
+func protocolParseData(data []byte) ([]*protocolElement, error) {
+	result := []*protocolElement{}
+	remaining := data
+
+	for len(remaining) > 0 {
+		remaining, fromLen, ok := extractShort(remaining)
+		if !ok {
+			return nil, errors.New("blah")
+		}
+		remaining, from, ok := extractFixedData(remaining, int(fromLen))
+		if !ok {
+			return nil, errors.New("blah2")
+		}
+		remaining, dataLen, ok := extractShort(remaining)
+		if !ok {
+			return nil, errors.New("blah3")
+		}
+		remaining, d, ok := extractFixedData(remaining, int(dataLen))
+		if !ok {
+			return nil, errors.New("blah4")
+		}
+		result = append(result, &protocolElement{from: string(from), data: string(d)})
+	}
+
+	return result, nil
+}
+
+func extractShort(d []byte) ([]byte, uint16, bool) {
+	if len(d) < 2 {
+		return nil, 0, false
+	}
+
+	return d[2:], uint16(d[0])<<8 |
+		uint16(d[1]), true
+}
+
+func extractFixedData(d []byte, l int) (newPoint []byte, data []byte, ok bool) {
+	if len(d) < l {
+		return d, nil, false
+	}
+	return d[l:], d[0:l], true
+}
